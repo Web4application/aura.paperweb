d158e4001b6120d7c8e521c0f5a8ccfa9a5832be
Web4 <167559384+Web4application@users.noreply.github.com>
2025-12-01 15:18:39 +0100

    Create QuantumLabScreen.tsx

---
+++ ui/screens/QuantumLabScreen.tsx
@@ -0,0 +1,8 @@
+export default function QuantumLabScreen() {
+  return (
+    <div className="aura-quantum">
+      <h2>Quantum Workspace</h2>
+      <p>Future home of .xlquantum circuits, qubits, gates, visualizers.</p>
+    </div>
+  );
+}
