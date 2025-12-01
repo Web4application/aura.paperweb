8ed05b5245bc93aac9f2ca3a8e97a6f1347aa8a7
Web4 <167559384+Web4application@users.noreply.github.com>
2025-12-01 15:17:29 +0100

    Create WorkspaceScreen.tsx

---
+++ ui/screens/WorkspaceScreen.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Terminal from "../aura_terminal/Terminal";
+
+export default function WorkspaceScreen() {
+  return (
+    <div className="aura-workspace">
+      <Terminal />
+    </div>
+  );
+}
